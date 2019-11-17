2ac3fe0e98a47318aed5759190be291cb693686b
Shahwar Spall <shevy.spall@gmail.com>
2019-11-17 05:29:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,7 +37,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+return programmer_hash [:alan_kay][:known_for]
 end
+
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -55,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+return programmer_hash[:dennis_ritchie][:languages]="C"
 end
 
 def adding_matz
@@ -79,7 +82,13 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+    programmer_hash[:yukihiro_matsumoto] = {
+    known_for: "Ruby",
+    languages:
+      ["LISP", "C"]
+    }
+    
+  programmer_hash
     
 end
 
@@ -102,6 +111,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+      programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash
      
 end
 
